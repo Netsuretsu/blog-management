d9154417953171f0d086a9ceee5cc93e505fa3a6
Netsuretsu <constantin-joval.awanchede@epitech.eu>
2023-08-03 23:57:14 +0100

    Register form created

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -19,4 +19,41 @@ router.get("/admin", async (req, res) => {
     }
 })
 
+router.post("/admin", async (req, res) => {
+
+    try {
+        locals = {
+            "title": "Admin",
+        }
+
+        const { username, password } = req.body;
+
+
+        res.render("admin/index", { locals, layout: adminLayout })
+    } catch (error) {
+        console.log(error)
+    }
+})
+router.post("/register", async (req, res) => {
+
+    try {
+        locals = {
+            "title": "Admin",
+        }
+
+        const { username, password } = req.body;
+
+        if (req.body.username === 'admin' && req.body.password === 'password') {
+            res.send("")
+        } else {
+            res.send('Wrong username or password');
+        }
+
+
+        res.render("admin/index", { locals, layout: adminLayout })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 module.exports = router;
